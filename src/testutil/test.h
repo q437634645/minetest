12b31beb7e19f2c1039266e49e64c8094f8e589a
log_alpha <z3981792@126.com>
2018-04-24 19:17:01 +0800

    set file output

--- src/testutil/test.h
+++ src/testutil/test.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <cerrno>
@@ -18,27 +19,20 @@ enum TestCaseType{
 
 class TestCase{
 public:
-	TestCase(TestCaseType tct,std::ostream &stream):
-		m_stream(stream),
-		type(tct)
-	{
+	TestCase(TestCaseType tct):type(tct){
 		startTime = porting::getTimeMs();
 	}
 	~TestCase(){
-		if(!m_stream.good())
-			return;
-		m_stream<<"TestCase = "<<(int)type<<"startTime ="<<startTime
-			<<"finishTime = "<<porting::getTimeMs()<<std::endl;
 	}
-	void stop(){
-		if(!m_stream.good())
-			return;
-		m_stream<<"TestCase = "<<(int)type<<"startTime ="<<startTime
-			<<" finishTime = "<<porting::getTimeMs()<<std::endl;
+	std::string stop(){
+		std::ostringstream os(std::ios_base::binary);
+		os<<"TestCase = "<<(int)type<<" "
+			"startTime = "<<startTime<<" "
+			"endTime = "<<getTimeMs()<<std::endl;
+		return os.c_str();
 	}
 private:
-	std::ostream &m_stream;
-	float startTime;
+	u64 startTime;
 	TestCaseType type;
 };
 
@@ -48,13 +42,13 @@ public:
 		m_testcase.clear();
 	}
 	void CreateTestCase(TestCaseType type){
-		m_testcase.push_back(TestCase(typem m_stream));
+		m_testcase.push_back(TestCase(type));
 	}
 	void FinishTestCase(){
 		std::vector<TestCase>::iterator i = m_testcase.begin();
 		if( i == m_testcase.end())
 			return;
-		i->stop();
+		m_stream<<i->stop();
 		m_testcase.erase(i);
 	}
 	void init(const std::string &filename){
