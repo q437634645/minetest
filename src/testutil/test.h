5398183478f9f1c662ff0253d717a56ca55d38ac
log_alpha <z3981792@126.com>
2018-05-01 17:10:42 +0800

    program testutil

--- src/testutil/test.h
+++ src/testutil/test.h
@@ -7,34 +7,14 @@
 #include <vector>
 #include <string>
 #include <cerrno>
+#include <map>
+#include <utility>
 #include "exceptions.h"
 #include "porting.h"
 
 class TestUtil;
 extern TestUtil *g_testutil;
 
-enum TestCaseType{
-	TCT_PlayerPos,
-	TCT_MAX,
-};
-
-class TestCase{
-public:
-	TestCase(TestCaseType tct):type(tct){
-		startTime = porting::getTimeMs();
-	}
-	~TestCase(){}
-	std::string stop(){
-		std::ostringstream os(std::ios_base::binary);
-		os<<"TestCase = "<<(int)type<<" "
-			"startTime = "<<startTime<<" "
-			"endTime = "<<porting::getTimeMs()<<std::endl;
-		return os.str();
-	}
-private:
-	u64 startTime;
-	TestCaseType type;
-};
 
 class TestUtil{
 public:
@@ -46,26 +26,7 @@ public:
 	void SetActive(bool _active){
 		active = _active;
 	}
-	void CreateTestCase(TestCaseType type){
-
-		if(!active)return;
-
-		m_testcase.push_back(TestCase(type));
-	}
-	void FinishTestCase(){
-
-		if(!active)return;
-
-		std::vector<TestCase>::iterator i = m_testcase.begin();
-		if( i == m_testcase.end())
-			return;
-		m_stream<<i->stop();
-		m_testcase.erase(i);
-	}
 	void init(const std::string &filename){
-
-		if(!active)return;
-
 		m_stream.open(filename.c_str(), std::ios::out);
 		if (!m_stream.good())
 			throw FileNotGoodException("Failed to open test file " +
@@ -74,11 +35,75 @@ public:
 	void OutputString(const std::string &str){
 		m_stream<<str<<std::endl;
 	}
+	void Count(const std::string &name, int value=1){
+		std::map<std::string,int>::iterator i;
+		i=m_count.find(name);
+		if(i==m_count.end())
+			m_count=value;
+		else i->second+=value;
+	}
+	void Avg(const std::string &name, float numerator,float denominator){
+		std::map<std::string,std::pair<float,float> >::iterator i;
+		i=m_average.find(name);
+		if(i==m_average.end()){
+			m_count=make_pair(numerator,denominator);
+		}
+		else {
+			std::pair<float,float>&p=i->second;
+			p.first+=numerator;
+			p.second+=denominator;
+		}
+	}
+	void Output(float curtime){
+		if(!m_stream.good())return;
+		std::map<std::string,int>::iterator i;
+		for(i=m_count.begin();i!=m_count.end();i++){
+			m_stream<<i->first<<" count:"<<i->second<<std::endl;
+		}
+		std::map<std::string,std::pair<float,float> >::iterator j;
+		for(j=m_average.begin();j!=m_average.end();j++){
+			float value=i->second.first;
+			if(i->second.second>1e-6)value = i->second.first/i->second.second;
+			m_stream<<i->first<<" average:"<<value<<std::endl;
+		}
+	}
 private:
 	bool active;
 	std::ofstream m_stream;
-	std::vector<TestCase>m_testcase;
+	std::map<std::string,int>m_count;
+	std::map<std::string,std::pair<float,float> >m_average;
 };
 
+
+enum TestCaseType{
+	TCT_AVG,
+	TCT_COUNT,
+};
+
+class TestCase{
+public:
+	TestCase(TestUtil *testutil,const string& name,TestCaseType m_type):
+		m_testutil(testutil),m_name(name)
+	{
+		m_starttime = porting::getTimeMs();
+	}
+	~TestCase(){
+		if(m_testutil==NULL)return;
+		u64 costtime = porting::getTimeMs() - m_starttime;
+		switch(m_type){
+			case TCT_AVG:
+				m_testutil->Count(m_name);
+				break;
+				m_testutil->Avg(m_name,costtime,1.0f);
+			case TCT_COUNT:
+				break;
+		}
+	}
+private:
+	std::string m_name;
+	u64 m_starttime;
+	TestCaseType m_type;
+	TestUtil *m_testutil;
+};
 
 #endif
