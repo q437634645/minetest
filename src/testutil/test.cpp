d10ab197d5591b612ea4d48dfdb19eef03c634b8
log_alpha <z3981792@126.com>
2018-05-04 20:32:40 +0800

    modify testutil for upload

--- src/testutil/test.cpp
+++ src/testutil/test.cpp
@@ -10,6 +10,6 @@ bool TestFileUpload(const std::string &filename,const std::string &testname){
 	fetch_request.post_fields["name"] = "@" + filename;
 	fetch_request.post_fields["TestName"] = testname;
 	fetch_request.multipart = true;
-	httpfetch_async(&fetch_request);
+	httpfetch_async(fetch_request);
 	return false;
 }
