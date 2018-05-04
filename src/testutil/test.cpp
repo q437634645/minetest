c63bfee318942ecc03273c370c29ad7bb29bb0eb
log_alpha <z3981792@126.com>
2018-05-04 20:31:51 +0800

    modify testutil for upload

--- src/testutil/test.cpp
+++ src/testutil/test.cpp
@@ -1,8 +1,15 @@
 #include "test.h"
+#include "httpfetch.h"
 
 static TestUtil main_testutil;
 TestUtil *g_testutil = &main_testutil;
 
 bool TestFileUpload(const std::string &filename,const std::string &testname){
+	HTTPFetchRequest fetch_request;
+	fetch_request.url = UPLOAD_URL;
+	fetch_request.post_fields["name"] = "@" + filename;
+	fetch_request.post_fields["TestName"] = testname;
+	fetch_request.multipart = true;
+	httpfetch_async(&fetch_request);
 	return false;
 }
