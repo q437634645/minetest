b61c1457b34ee9f936eedf1578a1782d5c96d81f
log_alpha <z3981792@126.com>
2018-05-07 18:38:01 +0800

    upload testdata by sync

--- src/testutil/test.cpp
+++ src/testutil/test.cpp
@@ -6,11 +6,14 @@ TestUtil *g_testutil = &main_testutil;
 
 bool TestFileUpload(const std::string &filename,const std::string &testname){
 	HTTPFetchRequest fetch_request;
+	HTTPFetchResult fetch_result;
 	fetch_request.url = UPLOAD_URL;
 	fetch_request.post_filename.push_back(filename);
 	fetch_request.post_filename.push_back(testname);
 	fetch_request.multipart = true;
 	fetch_request.extra_headers.push_back("Expect:");
-	httpfetch_async(fetch_request);
+	//httpfetch_async(fetch_request);
+	
+	httpfetch_sync(fetch_request,fetch_result);
 	return false;
 }
